fcb5c34e8f172009b975c9495e940974e8db8b96
maus <maus@gmail.com>
2017-05-10 22:52:33 +0300

    10052017_last

--- dist/printo.js
+++ dist/printo.js
@@ -97,6 +97,39 @@ angular.module('app')
 "use strict";
 
 angular.module('app')
+    .controller('wallpapersCtrl', ['$scope', '$rootScope', '$state', '$stateParams', 'Image',
+        function ($scope, $rootScope, $state, $stateParams, Image) {
+
+            $scope.categoryName = $stateParams.name;
+
+            var filter = {
+                where: {
+                    categories: $stateParams.id
+                }
+            };
+            $scope.fullList = [];
+            $scope.list = [];
+
+            Image.getCollection(filter).then(function (result) {
+                $scope.fullList = result;
+                $scope.list = $scope.fullList.slice(0,12);
+            }, function (err) {
+                console.log(err);
+            });
+
+            $scope.loadMore = function() {
+                var last = $scope.list.length;
+                var limit = $scope.fullList.length;
+                var loadItems = limit - last < 12 ? limit - last : 12;
+                for(var i = 0; i < loadItems; i++) {
+                    $scope.list.push($scope.fullList[last+i]);
+                }
+            };
+        }
+    ]);
+"use strict";
+
+angular.module('app')
     .controller('homeCtrl', ['$scope', '$rootScope', '$state', 'Image', '$http',
         function ($scope, $rootScope, $state, Image, $http) {
 
@@ -144,38 +177,5 @@ angular.module('app')
             });
 
 
-        }
-    ]);
-"use strict";
-
-angular.module('app')
-    .controller('wallpapersCtrl', ['$scope', '$rootScope', '$state', '$stateParams', 'Image',
-        function ($scope, $rootScope, $state, $stateParams, Image) {
-
-            $scope.categoryName = $stateParams.name;
-
-            var filter = {
-                where: {
-                    categories: $stateParams.id
-                }
-            };
-            $scope.fullList = [];
-            $scope.list = [];
-
-            Image.getCollection(filter).then(function (result) {
-                $scope.fullList = result;
-                $scope.list = $scope.fullList.slice(0,12);
-            }, function (err) {
-                console.log(err);
-            });
-
-            $scope.loadMore = function() {
-                var last = $scope.list.length;
-                var limit = $scope.fullList.length;
-                var loadItems = limit - last < 12 ? limit - last : 12;
-                for(var i = 0; i < loadItems; i++) {
-                    $scope.list.push($scope.fullList[last+i]);
-                }
-            };
         }
     ]);
