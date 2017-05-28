f6813f782c44005406cce614b03daa7ddcf1fa23
Andrey Vasilenko <0933322888@ukr.net>
2017-05-29 02:15:08 +0300

    contacts and delivery

--- app/components/payment/paymentCtrl.js
+++ app/components/payment/paymentCtrl.js
@@ -4,6 +4,8 @@ angular.module('app')
     .controller('paymentCtrl', ['$scope', '$rootScope', '$state',
         function ($scope, $rootScope, $state) {
 
-
+            $scope.findOrder = function () {
+                console.log("call to find order API")
+            }
         }
     ]);
